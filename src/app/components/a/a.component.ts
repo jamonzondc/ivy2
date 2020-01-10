319cadebcefe9093ec98b91eba43edf44b1569b5
jamonzondc <54319358+jamonzondc@users.noreply.github.com>
2020-01-10 17:07:20 -0500

    video

--- src/app/components/a/a.component.ts
+++ src/app/components/a/a.component.ts
@@ -1,18 +1,16 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
-import Plyr from 'plyr';
+
 
 @Component({
   selector: 'app-a',
   templateUrl: './a.component.html',
   styleUrls: ['./a.component.css']
 })
-export class AComponent implements OnInit, AfterViewInit {
+export class AComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
   }
-  ngAfterViewInit(): void {
-    const player = new Plyr('#player');
-  }
+ 
 }
--- src/app/segundo/b/b.component.ts
+++ src/app/segundo/b/b.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import Plyr from 'plyr';
 @Component({
   selector: 'app-b',
   templateUrl: './b.component.html',
   styleUrls: ['./b.component.css']
 })
-export class BComponent implements OnInit {
-
+export class BComponent implements OnInit, AfterViewInit {
+  
   b:string='Soy B-Interpolacion';
   constructor() { }
 
   ngOnInit() {
   }
-
+  ngAfterViewInit(): void {
+    const player = new Plyr('#player');
+  }
 }
